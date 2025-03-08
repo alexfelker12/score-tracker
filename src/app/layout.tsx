a405ce2e5f2f73356730355b4c3385891afd7c03
Alex Felker <alexanderfelker@gmx.net>
2025-03-08 13:48:05 +0100

    adjust dev environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased grid grid-rows-[auto_1fr_auto]`}
       >
+        <div></div>
         {children}
-      </body>
+        <div></div>
+        </body>
     </html>
   );
 }
