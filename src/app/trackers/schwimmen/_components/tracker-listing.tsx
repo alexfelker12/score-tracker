4d12148e7c03223182436fd894d7948471b922db
Alex Felker <alexanderfelker@gmx.net>
2025-03-13 23:12:24 +0100

    listing of existing tracker
    - added existing trackers in /trackers/schwimmen

---
+++ src/app/trackers/schwimmen/_components/tracker-listing.tsx
@@ -0,0 +1,59 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { delay, timeElapsed } from "@/lib/utils";
+import { getAllTrackersByArg } from "@/server/actions/trackerActions";
+import { Prisma, TrackerName } from "@prisma/client/edge";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
+
+export type TrackerListingType = {
+  trackerName: TrackerName
+}
+
+export const TrackerListing = async ({ trackerName }: TrackerListingType) => {
+  const trackers = await getAllTrackersByArg({
+    where: {
+      name: trackerName
+    }
+  })
+
+  await delay(2000)
+
+  if (trackers && trackers.data) return (
+    <div className="gap-4 grid md:grid-cols-2">
+      {trackers.data.map((tracker) => (
+        <TrackerCard key={tracker.id} {...tracker} />
+      ))}
+    </div>
+  );
+}
+
+export type TrackerCardType = Prisma.TrackerGetPayload<{}>
+export const TrackerCard = ({ id, name, createdAt }: TrackerCardType) => {
+  return (
+    <Link href={`/trackers/schwimmen/${id}`} className="group">
+      <Card className="group-hover:bg-accent justify-between gap-4 py-4 w-full h-full transition-all">
+        <CardHeader className="px-4">
+
+          <CardTitle className="text-lg leading-none">{name}</CardTitle>
+          <CardDescription className="leading-none">{timeElapsed(createdAt)}</CardDescription>
+
+        </CardHeader>
+        <CardContent className="flex justify-end">
+
+          {/* extra link to tracker */}
+          <div className="flex items-center">
+            <ArrowRightIcon className="transition-transform group-hover:translate-x-1" />
+          </div>
+
+        </CardContent>
+      </Card>
+    </Link>
+  )
+}
+
+export const TrackerCardLoading = () => {
+  return (
+    <Skeleton className="rounded-xl h-28"></Skeleton>
+  )
+}
--- src/app/trackers/schwimmen/page.tsx
+++ src/app/trackers/schwimmen/page.tsx
@@ -1,16 +1,32 @@
+import { Divide, LoaderCircleIcon } from "lucide-react";
+import { Suspense } from "react";
 import { ParticipantsForm } from "./_components/participants-form";
+import { TrackerCardLoading, TrackerListing } from "./_components/tracker-listing";
 
-export default function Schwimmen() {
+export default async function Schwimmen() {
   return (
     <main className="flex flex-col gap-6">
       <div>
         {/* heading + description */}
-        <h1 className="text-2xl">Schwimmen</h1>
-        <p className="text-muted-foreground text-sm">Choose all players participating in this round</p>
+        <h1 className="text-2xl">Create tracker for `Schwimmen`</h1>
+        {/* <p className="text-muted-foreground text-sm">Choose all players participating in this round</p> */}
       </div>
 
       {/* form - main content */}
       <ParticipantsForm minPlayers={2} maxPlayers={9} />
+
+      <div className="space-y-4">
+        <h2 className="font-medium text-lg">Available trackers for `Schwimmen`</h2>
+        <Suspense fallback={
+          <div className="gap-4 grid md:grid-cols-2">
+            {Array.from({ length: 2 }).map((_, idx) => (
+              <TrackerCardLoading key={idx} />
+            ))}
+          </div>
+        }>
+          <TrackerListing trackerName="SCHWIMMEN" />
+        </Suspense>
+      </div>
     </main>
   );
 }
