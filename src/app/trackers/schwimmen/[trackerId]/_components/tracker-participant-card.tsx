a3494af2b78711600f84825cc947c80d814a9b79
Alex Felker <alexanderfelker@gmx.net>
2025-03-15 15:59:10 +0100

    default size participant loader & header z-index
    - added default text size height of size "3" to participant card loader since the value saved to the session is not available at loading
    - added z-index to header element so its above participant cards
    - adjusted rights maps for icon sizes & conditions for showing "swimming" state

--- src/app/trackers/schwimmen/[trackerId]/_components/tracker-participant-card.tsx
+++ src/app/trackers/schwimmen/[trackerId]/_components/tracker-participant-card.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/ui/card";
 import { ManSwimmingIcon } from "@/components/ui/icons/man-swimming";
 import { SkullIcon } from "@/components/ui/icons/skull";
 import { Skeleton } from "@/components/ui/skeleton";
-import { SCHWIMMEN_TEXT_SIZE_MAP } from "@/lib/constants";
+import { SCHWIMMEN_ICON_SIZE_MAP, SCHWIMMEN_TEXT_SIZE_MAP } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import { Player } from "@/store/schwimmenSessionStore";
 import { HeartIcon } from "lucide-react";
@@ -22,26 +22,25 @@ export const ParticipantCard = ({ id, lifes, name, size, playerSwimming }: Parti
       )}
     >
       <div className="flex justify-between items-center gap-4">
-        <span
-          className={cn("text-sm",
-            SCHWIMMEN_TEXT_SIZE_MAP[size]
-          )}
-        >{name}</span>
+        {/* player name */}
+        <span className={cn("text-sm", SCHWIMMEN_TEXT_SIZE_MAP[size])}>{name}</span>
 
         <div className="flex gap-1 w-fit">
-          {playerSwimming === id && lifes > 0 ?
-            <ManSwimmingIcon className={SCHWIMMEN_TEXT_SIZE_MAP[size]} />
+          {/* swimming icon - stays also after death */}
+          {playerSwimming === id && <ManSwimmingIcon className={SCHWIMMEN_ICON_SIZE_MAP[size]} />}
+
+          {/* show hearts or skull icon(s) depending on amount of lifes */}
+          {lifes === 0 ?
+            <SkullIcon className={SCHWIMMEN_ICON_SIZE_MAP[size]} />
             :
-            lifes === 0 ?
-              <SkullIcon className={SCHWIMMEN_TEXT_SIZE_MAP[size]} />
-              :
-              Array.from({ length: lifes }).map((_, idx) => (
-                <HeartIcon key={idx}
-                  className={cn("size-6 stroke-red-500 fill-red-500",
-                    SCHWIMMEN_TEXT_SIZE_MAP[size]
-                  )}
-                />
-              ))
+            //* if player is swimming, do not display hearts
+            playerSwimming !== id && Array.from({ length: lifes }).map((_, idx) => (
+              <HeartIcon key={idx}
+                className={cn("size-6 stroke-red-500 fill-red-500",
+                  SCHWIMMEN_ICON_SIZE_MAP[size]
+                )}
+              />
+            ))
           }
         </div>
       </div>
@@ -50,13 +49,19 @@ export const ParticipantCard = ({ id, lifes, name, size, playerSwimming }: Parti
 }
 
 export type ParticipantCardLoadingType = {
-  size: number
+  size?: number
 }
 
 ParticipantCard.loading = ({ size }: ParticipantCardLoadingType) => {
   return (
     <Skeleton className="p-2 border border-transparent rounded-xl">
-      <div className={cn("invisible opacity-0", SCHWIMMEN_TEXT_SIZE_MAP[size])} aria-hidden="true">sizer text</div>
+      <div
+        className={cn(
+          "invisible opacity-0 text-2xl",
+          size && SCHWIMMEN_TEXT_SIZE_MAP[size]
+        )}
+        aria-hidden="true"
+      >sizer text</div>
     </Skeleton>
   )
 }
