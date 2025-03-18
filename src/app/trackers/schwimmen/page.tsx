19c283073154d76990f6e4ef69b94131aad825b0
Alex Felker <alexanderfelker@gmx.net>
2025-03-18 21:39:55 +0100

    Revert "Update page.tsx"
    This reverts commit 1c48c2b3ae0ed87b820aaef74b389cc482b4915c.

--- src/app/trackers/schwimmen/page.tsx
+++ src/app/trackers/schwimmen/page.tsx
@@ -1,7 +1,6 @@
 import { Breadcrumbs, BreadcrumbType } from "@/components/breadcrumbs";
 import { ParticipantsForm } from "./_components/participants-form";
 import { TrackerListing } from "./_components/tracker-listing";
-import { HydrationBoundary } from "@tanstack/react-query";
 
 export const dynamic = 'force-dynamic'
 
@@ -33,9 +32,7 @@ export default async function Schwimmen() {
         <h2 className="mt-2 font-bold text-xl">Available trackers for `Schwimmen`</h2>
 
         {/* see available trackers */}
-        <HydrationBoundary>
-          <TrackerListing trackerName="SCHWIMMEN" />
-        </HydrationBoundary>
+        <TrackerListing trackerName="SCHWIMMEN" />
       </div>
     </main>
   );
