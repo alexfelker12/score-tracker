73c4018120cfa8b3581a24c20e24afe3d2d80e10
Alex Felker <alexanderfelker@gmx.net>
2025-04-25 17:47:09 +0200

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,10 +5,13 @@ import { prisma } from "@/server/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
-  trustedOrigins: [
-    "http://localhost:3000",
-    "http://192.168.178.71:3000",
-  ],
+  //* developement
+  // trustedOrigins: [
+  //   "http://localhost:3000",
+  //   "http://192.168.178.71:3000",
+  // ],
+  //* production
+  trustedOrigins: [process.env.VERCEL_URL!],
   emailAndPassword: {
     enabled: true,
   },
